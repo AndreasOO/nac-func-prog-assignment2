fa2055c6784d3edb705c7512719140f9aa44f4b3
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-02-27 15:54:57 +0100

    Finished aoc 2020-3a but needs refactoring

--- src/part1/aoc_2020_3/a/Main.kt
+++ src/part1/aoc_2020_3/a/Main.kt
@@ -1,5 +1,40 @@
 package part1.aoc_2020_3.a
 
+import java.io.File
+
+data class Point(val x: Int, val y: Int)
+
 fun main() {
-    println("Hello World!")
+    val mapData = File("aoc-input/aoc-2020-3a-input.txt").readLines().toList()
+
+    val travelMap : MutableMap<Point, String> = HashMap()
+
+    var y = 0;
+    var x = 0;
+    for (line in mapData ) {
+        for (symbol in line.trim().split("")) {
+            if (symbol.isEmpty()) continue
+            travelMap.putIfAbsent(Point(x, y), symbol)
+            x++
+        }
+        y++
+        x = 0
+    }
+
+    x = 0
+    y = 0
+    var count = 0
+    while (y <= mapData.size-1) {
+        y++
+        x += 3
+        x %= 31
+        println("$x : $y  ${travelMap[Point(x, y)]}")
+        if (travelMap[Point(x, y)].equals("#")) {
+            count++
+        }
+    }
+
+    println(count) // should be 240
+
+
 }

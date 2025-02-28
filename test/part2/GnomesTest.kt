b693476d7a2a23e2e00bd18297c0af68b6d4540c
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-02-28 11:22:57 +0100

    Finished tests

--- test/part2/GnomesTest.kt
+++ test/part2/GnomesTest.kt
@@ -41,6 +41,75 @@ Tomten
 
 @org.junit.jupiter.api.Test
  fun allHierarchiesHappyFlow() {
-     assertEquals(setOf("Dammråttan"), findGnomesBelow("Skumtomten", hierarchyMapHappyFlow))
+ assertEquals(setOf("Glader", "Butter", "Tröger", "Trötter", "Blyger", "Rådjuret", "Nyckelpigan", "Haren",
+                    "Räven", "Skumtomten", "Gråsuggan", "Myran", "Dammråttan", "Bladlusen"),
+              findGnomesBelow("Tomten", hierarchyMapHappyFlow))
+
+ assertEquals(setOf("Tröger", "Trötter", "Skumtomten", "Dammråttan", "Blyger"), findGnomesBelow("Glader", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Tröger", hierarchyMapHappyFlow))
+
+ assertEquals(setOf("Skumtomten", "Dammråttan"), findGnomesBelow("Trötter", hierarchyMapHappyFlow))
+
+ assertEquals(setOf("Dammråttan"), findGnomesBelow("Skumtomten", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Dammråttan", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Blyger", hierarchyMapHappyFlow))
+
+ assertEquals(setOf("Rådjuret", "Nyckelpigan", "Haren", "Räven", "Gråsuggan", "Myran", "Bladlusen"),
+              findGnomesBelow("Butter", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Rådjuret", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Nyckelpigan", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Haren", hierarchyMapHappyFlow))
+
+ assertEquals(setOf("Gråsuggan", "Myran", "Bladlusen"), findGnomesBelow("Räven", hierarchyMapHappyFlow))
+
+ assertEquals(setOf<String>(), findGnomesBelow("Gråsuggan", hierarchyMapHappyFlow))
+
+ assertEquals(setOf("Bladlusen"), findGnomesBelow("Myran", hierarchyMapHappyFlow))
  }
+
+ @org.junit.jupiter.api.Test
+ fun fakeNamesInExistingMap() {
+  assertEquals(setOf<String>(), findGnomesBelow("TEST1", hierarchyMapHappyFlow))
+  assertEquals(setOf<String>(), findGnomesBelow("TEST2", hierarchyMapHappyFlow))
+  assertEquals(setOf<String>(), findGnomesBelow("TEST3", hierarchyMapHappyFlow))
+ }
+
+ @org.junit.jupiter.api.Test
+ fun allHierarchiesEmpty() {
+  assertEquals(setOf<String>(), findGnomesBelow("Tomten", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Glader", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Tröger", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Trötter", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Skumtomten", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Dammråttan", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Blyger", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Butter", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Rådjuret", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Nyckelpigan", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Haren", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Räven", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Gråsuggan", hierarchyMapEmpty))
+
+  assertEquals(setOf<String>(), findGnomesBelow("Myran", hierarchyMapEmpty))
+
+ }
+
 }
